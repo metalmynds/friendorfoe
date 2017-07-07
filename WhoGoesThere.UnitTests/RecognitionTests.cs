d8cb1985b1739e96ec6f81bd16142b47d87e0c08
Dave Paterson <crazeydave.2.0@gmail.com>
2017-07-07 05:52:20 +0100

    Added Unit Test Project for Who Goes There Service.

---
+++ WhoGoesThere.UnitTests/RecognitionTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WhoGoesThere.UnitTests
+{
+    [TestClass]
+    public class RecognitionTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
