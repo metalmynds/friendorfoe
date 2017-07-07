6a7ecdab19cb3cef5f7bbc3718cb6574f6ea65ca
Dave Paterson <crazeydave.2.0@gmail.com>
2017-07-07 04:52:45 +0100

    Added WatchMan App Project (Cross Platform)

---
+++ WatchManApp/WatchManApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace WatchManApp.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ WatchManApp/WatchManApp/Views/ItemsPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using WatchManApp.Models;
+using WatchManApp.ViewModels;
+
+using Xamarin.Forms;
+
+namespace WatchManApp.Views
+{
+	public partial class ItemsPage : ContentPage
+	{
+		ItemsViewModel viewModel;
+
+		public ItemsPage()
+		{
+			InitializeComponent();
+
+			BindingContext = viewModel = new ItemsViewModel();
+		}
+
+		async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+		{
+			var item = args.SelectedItem as Item;
+			if (item == null)
+				return;
+
+			await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
+
+			// Manually deselect item
+			ItemsListView.SelectedItem = null;
+		}
+
+		async void AddItem_Clicked(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new NewItemPage());
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			if (viewModel.Items.Count == 0)
+				viewModel.LoadItemsCommand.Execute(null);
+		}
+	}
+}
