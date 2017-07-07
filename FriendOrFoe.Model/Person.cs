916a6dd03897a2ff0ed4ec12a743e29da436c7fd
Dave Paterson <crazeydave.2.0@gmail.com>
2017-07-07 05:29:12 +0100

    Added Basic Data Model Project.

---
+++ FriendOrFoe.Model/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FriendOrFoe.Model
+{
+    public class Person
+    {
+        public Guid Identifier { get; set; }
+        public String Name { get; set; }
+        public Status Status { get; set; }
+    }
+}
